eaf492e818cec0810ad65bfc06d1c3834e4efced
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-06-15 14:38:07 +0100

    Fis the current index
    In this chnage, I remove dthe quote state and changed the currentQuote to currentIndex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,17 @@
 
 import Image from "next/image";
 import { useState, useEffect } from "react";
-const quotes = [
-  {
-    quote:
-      "“Education is the passport to the future, for tomorrow belongs to those who prepare for it today.”",
-  },
-  {
-    quote:
-      "“Education's purpose is to replace an empty mind with an open one..”",
-  },
-  {
-    quote:
-      "“Education makes a people easy to lead but difficult to drive: easy to govern, but impossible to enslave.”",
-  },
-  {
-    quote:
-      "“Knowledge is power. Information is liberating. Education is the premise of progress in every society, in every family..”",
-  },
-];
+import quotes from "./quotes.json";
 
 export default function Home() {
-  const [currentQuote, setCurrentQuote] = useState(0);
-  const [quote, setQuote] = useState(quotes[currentQuote].quote);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentQuote((prevQuote) => (prevQuote + 1) % quotes.length);
-      setQuote(quotes[currentQuote].quote);
+      setCurrentIndex((prevQuote) => (prevQuote + 1) % quotes.length);
     }, 5000);
     return () => clearInterval(intervalId);
-  }, [currentQuote]);
+  }, [currentIndex]);
 
   return (
     <div className="flex justify-center items-center flex-col pt-6">
@@ -39,7 +20,9 @@ export default function Home() {
         Drive the world through knowledge.
       </h2>
 
-      <h3 className="max-sm:text-center max-sm:pt-4 px-2">{quote}</h3>
+      <h3 className="max-sm:text-center max-sm:pt-4 px-2">
+        {quotes[currentIndex].quote}
+      </h3>
       <div className="flex justify-center items-center py-4 ">
         <button className="flex justify-center p-2 rounded text-white bg-[var(--accent-color)]">
           Get started
