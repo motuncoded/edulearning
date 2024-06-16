637d5ab7e1b887a362398006355fefca9df6dacd
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-06-16 20:39:42 +0100

    Hero intro section (#6)
    * Setup the nextjs project
    
    In this change I set up a nextjs typescript project configured with tailwindcss for styling and prettier for formating
    
    * Setup nextjs typescript project
    
    In this change, I setup this nextjs typescript project configured with tailwindcss and prettier
    
    * Deleting all installed files
    
    In this cahange, I remove all installed files that was causing conflict with the main branch
    
    * Setup the nextjs typescript project
    
    In this change, I setup the nextjs typescript project configured with tailwindcss for styling and prettier for formating the codebase
    
    * delete some unnecessary files
    
    In this change, I deleted next.svg and vercel.svg which is not necessary to the project
    
    * Created the navigation menu
    
    In this change, I create the navigation menu for the eduleaening and made responsive to view in mobiles as well as desktop
    
    * Fix the menu state and name convention
    
    In this change, I made an updater function in the toggle menu function and made changes in the naming convention using the pascalcase
    
    * Fix the menu state and name convention
    
    In this change, I made an updater function in the toggle menu function and made changes in the naming convention using the pascalcase
    
    * Create the Hero Into section
    
    In this change, I create an intro scetion for the hero page
    
    * Fis the current index
    
    In this chnage, I remove dthe quote state and changed the currentQuote to currentIndex
    
    * Fix the hero section image
    
    In this change I changed the hero section image type from png to svg in which the image was blurry
    
    ---------
    
    Co-authored-by: Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navigation/navbar/page";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
