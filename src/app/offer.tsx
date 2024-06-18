a28e3a3065e259be218e18e12d1cd4b51820e1c7
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-06-18 15:53:09 +0100

    feat: create what we offer section
    In this change, I create an what we offer section

--- src/app/offer.tsx
+++ src/app/offer.tsx
@@ -1,44 +1,67 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import { TiTick } from "react-icons/ti";
+import { GrCatalogOption } from "react-icons/gr";
+import { BiBookContent } from "react-icons/bi";
+import { GrResources } from "react-icons/gr";
+import { MdLibraryBooks } from "react-icons/md";
 
 export default function offer() {
   return (
-    <section className='flex justify-between items-center'>
-          <div>
-              <Image  src="/study.svg"  width={500} height={500}alt="studying"/>
+    <section className="flex justify-between items-center max-sm:flex-col p-4">
+      <Image src="/study.svg" width={500} height={500} alt="studying" />
+      <div>
+        <h4 className="text-3xl font-bold">
+          We offer valuable materials in different formats.
+        </h4>
+        <div className="flex flex-col ">
+          <div className="flex ">
+            <div className="py-4">
+              <GrCatalogOption
+                size="18"
+                className="text-[var(--accent-color)] "
+              />
+              <h5 className="font-bold"> Course Catalog</h5>
+              <h6>
+                A comprehensive catalog of courses, including descriptions,
+                duration, and level of difficulty
+              </h6>
+            </div>
+            <div className="py-4">
+              <BiBookContent
+                size="18"
+                className="text-[var(--primary-color)]"
+              />
+              <h5 className="font-bold"> Course Content</h5>
+              <h6>
+                Engaging and interactive course content, including video
+                lectures, quizzes, and assignments
+              </h6>
+            </div>
           </div>
-          <div>
-              <h4 className="text-3xl font-bold">We offer valuable materials in different formats</h4>
-              <div className="flex flex-col">
-              <div className="flex">
-                  <div>
-                      <TiTick className="bg-[var(--accent-color)] text-[var(--background-color)] rounded-full w-[20px] h-[20px]" />
-                      <h5> Course Catalog</h5>
-                      <h6>A comprehensive catalog of courses, including descriptions, duration, and level of difficulty</h6>
-                  </div>
-                  <div>
-                      <TiTick className="bg-[var(--primary-color)] text-[var(--background-color)] rounded-full w-[20px] h-[20px]" />
-                      <h5> Course Catalog</h5>
-                      <h6>A comprehensive catalog of courses, including descriptions, duration, and level of difficulty</h6>
-                  </div>
-                  </div>
-                  <div className="flex">
-                  <div>
-                      <TiTick className="bg-[var(--active-color)] text-[var(--background-color)] rounded-full w-[20px] h-[20px]" />
-                      <h5> Course Catalog</h5>
-                      <h6>A comprehensive catalog of courses, including descriptions, duration, and level of difficulty</h6>
-                  </div>
-                  <div>
-                      <TiTick className="bg-[var(--secondary-color)] text-[var(--background-color)] rounded-full w-[20px] h-[20px]" />
-                      <h5> Course Catalog</h5>
-                      <h6>A comprehensive catalog of courses, including descriptions, duration, and level of difficulty</h6>
-                      </div>
-                      </div>
+          <div className="flex">
+            <div className="py-4">
+              <GrResources size="18" className="text-[var(--active-color)]" />
+              <h5 className="font-bold"> Resources</h5>
+              <h6>
+                Dedicated support team to assist users with technical issues and
+                learning-related queries.
+              </h6>
+            </div>
+            <div className="py-4">
+              <MdLibraryBooks
+                size="18"
+                className="text-[var(--secondary-color)]"
+              />
+              <h5 className="font-bold"> Content Library</h5>
+              <h6>
+                A vast library of learning content, including articles, videos,
+                podcasts, and infographics
+              </h6>
+            </div>
           </div>
-          </div>
-
-          
+        </div>
+      </div>
     </section>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import quotes from "./quotes.json";
-import Offer from "./offer"
-
+import Offer from "./offer";
 
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,26 +17,30 @@ export default function Home() {
 
   return (
     <div>
-    <div className="flex justify-center items-center flex-col pt-6">
-      <h2 className="text-[3rem]  font-bold max-sm:text-[2.5rem]	text-center max-sm:leading-9	">
-        Drive the world through knowledge.
-      </h2>
+      <div className="flex justify-center items-center flex-col pt-6">
+        <h2 className="text-[3rem]  font-bold max-sm:text-[2.5rem]	text-center max-sm:leading-9	">
+          Drive the world through knowledge.
+        </h2>
 
-      <h3 className="max-sm:text-center max-sm:pt-4 px-2">
-        {quotes[currentIndex].quote}
-      </h3>
-      <div className="flex justify-center items-center py-4 ">
-        <button className="flex justify-center p-2 rounded text-white bg-[var(--accent-color)]">
-          Get started
-        </button>
-      </div>
-      <div className="pt-6">
-        <Image src="/learning.svg" width={600} height={600} alt="edulearning" />
-      </div>
+        <h3 className="max-sm:text-center max-sm:pt-4 px-2">
+          {quotes[currentIndex].quote}
+        </h3>
+        <div className="flex justify-center items-center py-4 ">
+          <button className="flex justify-center p-2 rounded text-white bg-[var(--accent-color)]">
+            Get started
+          </button>
+        </div>
+        <div className="pt-6">
+          <Image
+            src="/learning.svg"
+            width={600}
+            height={600}
+            alt="edulearning"
+          />
+        </div>
       </div>
 
-    <Offer/>
-
+      <Offer />
     </div>
   );
 }
