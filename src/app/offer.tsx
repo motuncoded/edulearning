6c92c69e6fa7486287e229cc6bc170ebe2de5cc4
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-07-01 15:06:41 +0100

    remove the import react and improve the videos section code base
    In this change I upadate and improve the codebase

--- src/app/offer.tsx
+++ src/app/offer.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Image from "next/image";
-import { TiTick } from "react-icons/ti";
 import { GrCatalogOption } from "react-icons/gr";
 import { BiBookContent } from "react-icons/bi";
 import { GrResources } from "react-icons/gr";
 import { MdLibraryBooks } from "react-icons/md";
 
-export default function offer() {
+type OfferIconProps = {
+  icon: React.ReactElement; // Change this from string to ReactElement
+  label: string;
+  description: string;
+};
+const OfferIcon: React.FC<OfferIconProps> = ({ icon, label, description }) => (
+  <div className="py-4 pr-4">
+    {icon}
+    <h5 className="font-bold">{label}</h5>
+    <h6>{description}</h6>
+  </div>
+);
+
+const Offer = () => {
   return (
     <section className="flex justify-between items-center max-sm:flex-col p-4 max-md:flex-col max-lg:flex-col max-xl:flex-col">
       <Image src="/study.svg" width={500} height={500} alt="studying" />
@@ -14,54 +26,35 @@ export default function offer() {
         <h4 className="text-3xl font-bold">
           We offer valuable materials in different formats.
         </h4>
-        <div className="flex flex-col ">
-          <div className="flex ">
-            <div className="py-4 pr-4">
-              <GrCatalogOption
-                size="18"
-                className="text-[var(--accent-color)] "
-              />
-              <h5 className="font-bold"> Course Catalog</h5>
-              <h6>
-                A comprehensive catalog of courses, including descriptions,
-                duration, and level of difficulty
-              </h6>
-            </div>
-            <div className="py-4">
-              <BiBookContent
-                size="18"
-                className="text-[var(--primary-color)]"
-              />
-              <h5 className="font-bold"> Course Content</h5>
-              <h6>
-                Engaging and interactive course content, including video
-                lectures, quizzes, and assignments
-              </h6>
-            </div>
+        <div className="flex flex-col">
+          <div className="flex">
+            <OfferIcon
+              icon={<GrCatalogOption size={18} color="var(--accent-color)" />}
+              label="Course Catalog"
+              description="A comprehensive catalog of courses, including descriptions, duration, and level of difficulty"
+            />
+            <OfferIcon
+              icon={<BiBookContent size={18} color="var(--primary-color)" />}
+              label="Course Content"
+              description="Engaging and interactive course content, including video lectures, quizzes, and assignments"
+            />
           </div>
           <div className="flex">
-            <div className="py-4  pr-4">
-              <GrResources size="18" className="text-[var(--active-color)]" />
-              <h5 className="font-bold"> Resources</h5>
-              <h6>
-                Dedicated support team to assist users with technical issues and
-                learning-related queries.
-              </h6>
-            </div>
-            <div className="py-4">
-              <MdLibraryBooks
-                size="18"
-                className="text-[var(--secondary-color)]"
-              />
-              <h5 className="font-bold"> Content Library</h5>
-              <h6>
-                A vast library of learning content, including articles, videos,
-                podcasts, and infographics
-              </h6>
-            </div>
+            <OfferIcon
+              icon={<GrResources size={18} color="var(--active-color)" />}
+              label="Resources"
+              description="Dedicated support team to assist users with technical issues and learning-related queries."
+            />
+            <OfferIcon
+              icon={<MdLibraryBooks size={18} color="var(--secondary-color)" />}
+              label="Content Library"
+              description="A vast library of learning content, including articles, videos, podcasts, and infographics"
+            />
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
+
+export default Offer;
