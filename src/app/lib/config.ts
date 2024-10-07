e88c7f0bd9eb7706236a441d93519c9bfc23d04b
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-10-07 05:33:40 +0100

    create the sign up component

---
+++ src/app/lib/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCukoU1hBMoTtUFdcUqCHuRJnlHH8OcDQw",
+  authDomain: "edulearning-61ffe.firebaseapp.com",
+  projectId: "edulearning-61ffe",
+  storageBucket: "edulearning-61ffe.appspot.com",
+  messagingSenderId: "808015495644",
+  appId: "1:808015495644:web:56ccd114f14205b8c80f19",
+  measurementId: "G-3GKZGXSPDP",
+};
