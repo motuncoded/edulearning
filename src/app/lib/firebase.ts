d055e48e227e0ac267e1d653a8bf897f1eb5b36e
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-10-08 13:03:26 +0100

    deleted the .env.local

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -13,7 +13,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
