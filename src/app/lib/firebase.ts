a7ff8275ac98fb6fcb2ffe8adadef3b5c3b8da2d
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-10-10 09:07:56 +0100

    add export the firebase.config

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+ export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
