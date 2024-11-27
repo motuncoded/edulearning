ab8e3ccb3ae07445cbb4146ed31e929c8b87b1aa
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-11-27 22:55:31 +0100

    feat: set up about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,25 @@
+
+
+import Image from "next/image";
+
 function AboutPage() {
   return (
-    <div>
-      <p>About page</p>
-    </div>
+    <section className="my-10">
+      <h2 className="mb-2 px-2">About us</h2>
+      <h3 className="w-3/4 text-6xl  font-medium mb-4">Making standard on the 
+        <span className="text-gray-500"> collection of materials.</span></h3>
+        <div className="mt-6 relative">
+          <Image src="/reading.jpg" alt="reading"   width={800}
+          height={600}
+        className="rounded-lg"/>
+        <div className="absolute top-32 left-2/4 bg-white h-60 w-80 rounded-md shadow-lg p-4 flex justify-center items-center flex-col">
+          <h3 className="font-bold mb-2">Our Misson</h3>
+          <p> We empower students and educators through collaborative learning. To aim at providing quick, reliable, and accessible learning materials to study challenges while fostering a community of knowledge sharing and academic growth.
+
+</p>
+        </div>
+        </div>
+    </section>
   );
 }
 
