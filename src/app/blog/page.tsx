741d151d285d1843eb18ef37d04c27df8f70910d
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-12-04 05:55:38 +0100

    feat:set up the blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,15 @@
 function BlogPage() {
   return (
-    <div>
-      <h1>Blog post</h1>
+    <div className="flex justify-center items-center flex-col font-bold min-h-[80vh]">
+      <h1 className="text-3xl mb-4">Inside Edulearning: Tips and Tricks</h1>
+      <h2 className="font-normal">Subscribe now to learn about different categories of education</h2>
+      <form action="" className="mb-4">
+        <label htmlFor="email" className="sr-only"> </label>
+        <input type="email" placeholder="Enter your email"
+          className="mt-8 border px-8 py-2 rounded-lg" />
+        <button>Subscibe</button>
+      </form>
+
     </div>
   );
 }
