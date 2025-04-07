b950d7b478a3a8b54f77c2a2894413f90715a3cf
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-04-07 18:22:38 +0100

    refractor : update the .env.local with necessary api keys

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.NEXT_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error("SUPABASE_URL and SUPABASE_ANON_KEY must be defined");
-}
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
