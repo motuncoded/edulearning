2bdd5044f8903b516abe163c1d6219b2d4b357b3
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-04-07 16:09:34 +0100

    refractor: hide the api key in the .env-local which I forgot

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -1,5 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error("SUPABASE_URL and SUPABASE_ANON_KEY must be defined");
+}
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
